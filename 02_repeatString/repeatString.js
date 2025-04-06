747f4a14269627c2509bdb65b0417bfbf1ab9314
Jeremiah Wilson <jewil@pm.me>
2025-04-06 18:30:25 -0400

    Finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(repeat, count) {
+  if (count < 0) { return "ERROR"; }
+  let repeated = "";
 
+  for (i = 0; i < count; i++) {
+    repeated += repeat;
+  }
+
+  return repeated;
 };
 
 // Do not edit below this line
