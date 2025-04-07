9f3f2256546813a5c4fc4062de3e37503a523c7b
Jeremiah Wilson <jewil@pm.me>
2025-04-06 23:10:16 -0400

    Finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) { return "ERROR"; }
 
+  let larger = a > b ? a : b;
+  let smaller = a < b ? a : b;
+
+  let sum = 0;
+  
+  for (i = smaller; i <= larger; i++) { sum += i; }
+
+  return sum;
 };
 
 // Do not edit below this line
