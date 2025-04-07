84d7388b184003e67c7d23387e75a6798ff48a0e
Jeremiah Wilson <jewil@pm.me>
2025-04-06 22:59:33 -0400

    Finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+function removeFromArray(arr, ...args) {
+  let ret = [];
+  for (i = 0; i < arr.length; i++) {
+    if (!args.includes(arr[i])) { ret.push(arr[i]); }
+  }
+  return ret;
 };
 
 // Do not edit below this line
