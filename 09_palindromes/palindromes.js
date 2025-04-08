cdcede6847f22ab3f7ce60df930490789f812620
Jeremiah Wilson <jewil@pm.me>
2025-04-07 20:24:45 -0400

    Finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // Clean the string: lowercase and remove non-alphanumeric characters
+  const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+  // Convert to array, reverse it, and join back to string
+  const reversed = cleanStr.split('').reverse().join('');
+  
+  // Compare
+  return cleanStr === reversed;
 };
 
 // Do not edit below this line
