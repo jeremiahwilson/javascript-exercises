16597ee9606e5d47f6766cd4e73d2b52075cda32
Jeremiah Wilson <jewil@pm.me>
2025-04-07 20:05:52 -0400

    Finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a-b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (let i=0; i<arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(n, e) {
+  return n**e;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let fact = 1;
+  for (let i = n; i > 0; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
