932972c38c5723d7fc76ddbf64f912b0aa1d981a
Jeremiah Wilson <jewil@pm.me>
2025-04-09 21:14:33 -0400

    Finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  let x = toNumber(n);
+  if (x < 0) { return "ERROR"; }
+  let fib = [1, 1];
+  let i = 2;
 
+  while(fib.length < x) {
+    fib[i] = fib[i-1]+fib[i-2];
+    i++;
+  }
+
+  return fib[x];
 };
 
 // Do not edit below this line
