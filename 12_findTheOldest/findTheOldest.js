6704230118da1335e01ebcccc52d70f3be55005c
Jeremiah Wilson <jewil@pm.me>
2025-04-09 21:36:55 -0400

    Finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let oldest = people[0];
 
+  for (let i = 1; i < people.length; i++) {
+    oldest = getAge(oldest) < getAge(people[i]) ? people[i] : oldest;
+  }
+
+  return oldest;
+};
+
+const getAge = function(person) {
+  return person.yearOfDeath != null ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth;
 };
 
 // Do not edit below this line
